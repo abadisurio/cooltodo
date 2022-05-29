6a1e2bc5a502f9ffb63217928314e7f7b1f0b607
Abadi Suryo <abadisuryosetiyo@gmail.com>
2022-05-29 23:16:06 +0700

    cooltodo: add redux to todo

---
+++ src/components/DisplayTodos.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { addTodos, removeTodos, updateTodos, completeTodos } from '../redux/reducer'
+import TodoItem from './TodoItem'
+
+
+const mapStateToProps = (state) => {
+    return {
+        todos: state
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addTodo: (obj) => { dispatch(addTodos(obj)) },
+        removeTodo: (id) => { dispatch(removeTodos(id)) },
+        updateTodo: (obj) => { dispatch(updateTodos(obj)) },
+        completeTodo: (id) => { dispatch(completeTodos(id)) },
+    }
+}
+
+const DisplayTodos = (props) => {
+
+    const [sort, setSort] = useState("active")
+
+    return (
+        <div>
+            <div>
+                <button onClick={() => setSort("active")}>
+                    Active
+                </button>
+                <button onClick={() => setSort("completed")}>
+                    Completed
+                </button>
+                <button onClick={() => setSort("all")}>
+                    All
+                </button>
+            </div>
+            <ul>
+                {(props.todos.length > 0 && sort === "all")
+                    ? props.todos.map((item) => {
+                        return <TodoItem
+                            key={item.id}
+                            item={item}
+                            removeTodo={props.removeTodo}
+                            updateTodo={props.updateTodo}
+                            completeTodo={props.completeTodo}
+                        />
+
+                    })
+                    : (props.todos.length > 0 && sort === "active")
+                        ? props.todos.map((item) => {
+                            return !item.completed && (
+                                <TodoItem
+                                    key={item.id}
+                                    item={item}
+                                    removeTodo={props.removeTodo}
+                                    updateTodo={props.updateTodo}
+                                    completeTodo={props.completeTodo}
+                                />
+                            )
+                        })
+                        : (props.todos.length > 0 && sort === "completed")
+                        && props.todos.map((item) => {
+                            return item.completed && (
+                                <TodoItem
+                                    key={item.id}
+                                    item={item}
+                                    removeTodo={props.removeTodo}
+                                    updateTodo={props.updateTodo}
+                                    completeTodo={props.completeTodo}
+                                />
+                            )
+                        })
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DisplayTodos)
